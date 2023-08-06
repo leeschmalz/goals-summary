ed220ff880e6cae2b992d6fdf58c790a9049ae0f
Lee Schmalz <leeschmalz@gmail.com>
2023-08-06 14:59:45 -0500

    Create get.py

---
+++ get.py
@@ -0,0 +1,42 @@
+import requests
+import os
+
+def getExistAttribute(attribute, count):
+    exist_token = os.environ.get("EXIST_TOKEN")
+    results = []
+    
+    page_size = 100  # Assuming the maximum page size is 100
+
+    page = 1
+    last_page = 999
+    while page <= last_page:
+        
+        searchParams = {
+            "attribute": attribute,
+            "limit": page_size,
+            "page": page
+        }
+        
+        url = "https://exist.io/api/2/attributes/values/?"
+        response = requests.get(url, params=searchParams, headers={
+            "Authorization": f"Token {exist_token}"
+        })
+
+        if page==1:
+            last_page = (response.json()['count'] + page_size - 1) // page_size
+
+        if response.status_code == 200:
+            data = response.json()
+            results.extend(data['results'])
+        else:
+            print(f"Failed with status code: {response.status_code}")
+            return None
+
+        page += 1
+
+    return results[:count]
+
+if __name__ == "__main__":
+    # attributes: 'workouts_distance', 'weight_lifting', 'pages_read', 'alcoholic_drinks'
+    data = getExistAttribute('pages_read', count=5)
+    print(data)
